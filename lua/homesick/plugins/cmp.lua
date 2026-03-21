6993a65e57ec02385b459c24f8ae44ca107dc8f0
Amir Aminbeidokhti <amir.aminbeidokhti@koho.ca>
2026-03-21 10:45:23 -0600

    refactor cmp for moon variant

--- lua/homesick/plugins/cmp.lua
+++ lua/homesick/plugins/cmp.lua
@@ -12,32 +12,43 @@ function M.get(variant)
   local selected = resolve_variant(variant)
   local palette = require("homesick.palette").get(selected)
   local is_dark_ui = selected == "night" or selected == "galaxy"
+  local is_moon = selected == "moon"
 
   local menu_bg = is_dark_ui and palette.bg or palette.pmenu_bg
   local sel_bg = is_dark_ui and palette.float_bg or palette.cyan
   local sel_fg = is_dark_ui and palette.cyan or palette.bg
-  local match_fg = is_dark_ui and palette.cyan or palette.blue
+  local option_fg = is_moon and palette.softcream or palette.bar_faded_text
+  local match_fg = is_moon and palette.white or (is_dark_ui and palette.cyan or palette.blue)
+
+  local kind_default = is_moon and option_fg or palette.cyan
+  local kind_function = is_moon and option_fg or palette.rose
+  local kind_variable = is_moon and option_fg or palette.text
+  local kind_class = is_moon and option_fg or palette.yellow
+  local kind_module = is_moon and option_fg or (palette.foam or palette.cyan)
+  local kind_snippet = is_moon and option_fg or palette.magenta
+  local kind_keyword = is_moon and option_fg or palette.purple
+  local kind_file = is_moon and option_fg or palette.blue
 
   return {
-    CmpItemAbbr = { fg = palette.bar_faded_text, bg = "NONE" },
+    CmpItemAbbr = { fg = option_fg, bg = "NONE" },
     CmpItemAbbrDeprecated = { fg = palette.comment, bg = "NONE", strikethrough = true },
     CmpItemAbbrMatch = { fg = match_fg, bg = "NONE", bold = true },
     CmpItemAbbrMatchFuzzy = { fg = match_fg, bg = "NONE", bold = true },
 
-    CmpItemKind = { fg = palette.cyan, bg = "NONE" },
-    CmpItemKindFunction = { fg = palette.rose, bg = "NONE" },
-    CmpItemKindMethod = { fg = palette.rose, bg = "NONE" },
-    CmpItemKindConstructor = { fg = palette.rose, bg = "NONE" },
-    CmpItemKindVariable = { fg = palette.text, bg = "NONE" },
-    CmpItemKindClass = { fg = palette.yellow, bg = "NONE" },
-    CmpItemKindInterface = { fg = palette.yellow, bg = "NONE" },
-    CmpItemKindModule = { fg = palette.foam or palette.cyan, bg = "NONE" },
-    CmpItemKindSnippet = { fg = palette.magenta, bg = "NONE" },
-    CmpItemKindKeyword = { fg = palette.purple, bg = "NONE" },
-    CmpItemKindFile = { fg = palette.blue, bg = "NONE" },
-    CmpItemKindFolder = { fg = palette.blue, bg = "NONE" },
+    CmpItemKind = { fg = kind_default, bg = "NONE" },
+    CmpItemKindFunction = { fg = kind_function, bg = "NONE" },
+    CmpItemKindMethod = { fg = kind_function, bg = "NONE" },
+    CmpItemKindConstructor = { fg = kind_function, bg = "NONE" },
+    CmpItemKindVariable = { fg = kind_variable, bg = "NONE" },
+    CmpItemKindClass = { fg = kind_class, bg = "NONE" },
+    CmpItemKindInterface = { fg = kind_class, bg = "NONE" },
+    CmpItemKindModule = { fg = kind_module, bg = "NONE" },
+    CmpItemKindSnippet = { fg = kind_snippet, bg = "NONE" },
+    CmpItemKindKeyword = { fg = kind_keyword, bg = "NONE" },
+    CmpItemKindFile = { fg = kind_file, bg = "NONE" },
+    CmpItemKindFolder = { fg = kind_file, bg = "NONE" },
 
-    CmpItemMenu = { fg = palette.comment, bg = "NONE", italic = true },
+    CmpItemMenu = { fg = is_moon and option_fg or palette.comment, bg = "NONE", italic = true },
 
     PmenuSel = { fg = sel_fg, bg = sel_bg, bold = true },
     CmpPmenuSel = { fg = sel_fg, bg = sel_bg, bold = true },
